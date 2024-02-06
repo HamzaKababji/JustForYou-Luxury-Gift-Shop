087b569b2c0ff984a4f94b15730f9b920a9d7f39
Hamza Elkababji <hamza.kababji@gmail.com>
2024-02-06 12:59:59 -0500

    Initial Commit - 50% Done

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+(function ($) {
+    "use strict";
+
+    $(document).ready(function($){
+
+        // stikcy js
+        $("#sticker").sticky({
+            topSpacing: 0
+        });
+
+        //mean menu
+        $('.main-menu').meanmenu({
+            meanMenuContainer: '.mobile-menu',
+            meanScreenWidth: "992"
+        });
+        
+         // search form
+        $(".search-bar-icon").on("click", function(){
+            $(".search-area").addClass("search-active");
+        });
+
+        $(".close-btn").on("click", function() {
+            $(".search-area").removeClass("search-active");
+        });
+    
+    });
+
+
+    jQuery(window).on("load",function(){
+        jQuery(".loader").fadeOut(1000);
+    });
+
+
+}(jQuery));
